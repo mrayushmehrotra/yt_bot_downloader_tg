279b2439f4d70df19d3251f11bc09d0920db39e9
mrayushmehrotra <ayushmehrotra099@gmail.com>
2024-04-27 21:26:51 +0530

    added user info add in a txt file

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+const fs = require("fs");
 const TelegramBot = require("node-telegram-bot-api");
 const token = "7099528174:AAHNoOq1bESF5ApDbX4S3tTTVMb8M06wM9c";
 const bot = new TelegramBot(token, { polling: true });
@@ -14,6 +14,27 @@ bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
   const messageText = msg.text;
   if (messageText === "/start") {
+    let user =
+      msg.chat.id +
+      " \n name: " +
+      msg.chat.first_name +
+      " " +
+      msg.chat.last_name +
+      " \n username: " +
+      msg.from.username +
+      " \n is bot:" +
+      msg.from.is_bot +
+      " \n language_code: " +
+      msg.from.language_code;
+
+    fs.writeFile("user.txt", user, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("File written successfully");
+        console.log(fs.readFileSync("user.txt", "utf8"));
+      }
+    });
     bot.sendMessage(chatId, "Hello, please send me YouTube video link");
   } else if (messageText.startsWith("http")) {
     try {
@@ -25,9 +46,7 @@ bot.on("message", async (msg) => {
       //    .then((video) => {
       bot.sendMessage(chatId, videoUrl);
       //  });
-      console.log(info);
     } catch (error) {
-      console.log("Error in fetching", error);
       bot.sendMessage(chatId, "sorry, there was error fetching video");
     }
   }
